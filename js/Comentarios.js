2d044b630d39dbad6ef61acd5f07fcb4f1f20c00
lucas <lucasmunizteles@gmail.com>
2023-10-21 19:09:10 -0300

    .

---
+++ js/Comentarios.js
@@ -0,0 +1,63 @@
+class Comentarios extends HTMLElement{
+    constructor(){
+        super();
+        const shadow = this.attachShadow({mode: "open"});
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
+    }
+    build(){
+
+   
+        const componetRoot = document.createElement("div");
+        componetRoot.setAttribute("class","container")
+        
+        const componet = document.createElement("div");
+        componet.setAttribute("class","comentarios")
+        
+        const autor = document.createElement("strong");
+        autor.textContent = this.getAttribute("autor");
+
+        const paragraph = document.createElement("p");
+        paragraph.textContent = this.getAttribute("paragraph");
+
+        const fonte = document.createElement("span");
+        fonte.textContent = this.getAttribute("fonte");
+
+        componet.appendChild(autor);
+        componet.appendChild(paragraph);
+        componet.appendChild(fonte);
+        componetRoot.appendChild(componet);
+
+        return componetRoot;
+    }
+    styles(){
+        const card = document.createElement("style");
+        card.textContent =`
+        .container{
+            font-size: 1.07em;
+            text-align: center;
+        }
+
+         .container strong{
+            margin-top:-10px
+            text-align: center;
+         }
+      
+        .container p {
+            display: flex;
+            text-align: center;
+            margin-top: 20px;
+            flex-direction: column;
+        }
+        
+        .comentarios span{
+            color: red;
+        }
+        `;
+
+        return card;
+    }
+
+
+}
+customElements.define('anotacao-2',Comentarios);
