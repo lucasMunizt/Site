3d9e3d2f5a1f66629ed3fa5eae6f2e9a5bb33763
lucas <lucasmunizteles@gmail.com>
2023-11-02 20:46:57 -0300

    conclusão

--- js/Comentarios.js
+++ js/Comentarios.js
@@ -52,6 +52,70 @@ class Comentarios extends HTMLElement{
 customElements.define('anotacao-2',Comentarios);
 
 
+    // let slider = document.querySelector('.slides');
+    // let sliders = document.querySelector('.slide');
+    // let contador=0;
+    // let pressend = false;
+    // let startx;
+    // let x;
+    // slider.addEventListener('mousedown',(e)=>{
+    //     pressend = true;
+    //     startx = e.offsetX - sliders.offsetLeft;
+    //     slider.style.cursor = 'grabbing'
+    // })
+
+    // slider.addEventListener('mouseenter',(e)=>{
+    //     slider.style.cursor = 'grab' 
+    // })
+
+    // //  slider.addEventListener('mouseleave',()=>{
+    // //      slider.style.cursor = 'default' 
+    // //  })
+
+    // slider.addEventListener('mouseup',(e)=>{
+    //     slider.style.cursor = 'grab' 
+    // })
+
+    //  slider.addEventListener('mouseup',(e)=>{
+    //      pressend = false; 
+    //  })
 
 
+    // slider.addEventListener('mousemove',(e)=>{
+    //     contador++;
+    //     if(!pressend) return;
+    //     e.preventDefault();
+
+    //     x = e.offsetX;
+
+    //     sliders.style.trasform = `translateX(${-contador * 100})%`;
+    //     Checkboundary()
+    // })
+
+    // function Checkboundary(){
+    //     let outer = slider.getBoundingClientRect();
+    //     let inner = sliders.getBoundingClientRect();
+        
+    //     if(parseInt(sliders.style.trasform)>0){
+    //         contador = 0;
+    //         sliders.style.trasform = '0px';
+    //     }
+    //     // else if(inner.right < outer.right){
+    //     //     sliders.style.left = `-${inner.width - outer.width}px`;
+    //     // }
+
+    // }
+   /* function ClikProximo(){
+        let startx;
+         let x;
+        let slider = document.querySelector('.slides');
+         let sliders = document.querySelector('.slide');
+         teste = false;
+         slider.addEventListener('click',(e)=>{
+                 teste = true;
+                 console.log()
+             })
+         
+    }*/
+    
    
